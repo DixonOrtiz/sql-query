13f0ecfe215afe8c781d8e05a4cf6a6fab3cd211
DixonOrtiz <dixon.ortizc@gmail.com>
2020-12-31 17:35:19 -0300

    query corrected

--- query.sql
+++ query.sql
@@ -4,5 +4,5 @@ ON employees.country_id = countries.id
 INNER JOIN continents
 ON countries.continent_id = continents.id
 SET employees.salary = employees.salary * continents.anual_adjustment
-WHERE employees.salary > 5000
+WHERE employees.salary <= 5000
 
